027579b219d94250bb19b19e8b3d7f557f030609
alesom.zorzi@gmail.com <alesom.zorzi@gmail.com>
2016-08-31 09:23:00 -0300

    Alterado scriptBanco.sql e testada a criação do banco.

--- sql/scriptBanco.sql
+++ sql/scriptBanco.sql
@@ -2,8 +2,6 @@ create database askandtrust;
 
 \c askandtrust
 
-create table usuario
-
 create table usuario (
    CPF   integer not null constraint pk_usuario primary key,
    email varchar(100) not null,
