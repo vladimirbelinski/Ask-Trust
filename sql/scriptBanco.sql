8ea71dd4873bf5a91736fd37214e29e56fb73b84
alesom.zorzi@gmail.com <alesom.zorzi@gmail.com>
2016-08-31 09:45:04 -0300

    Alterado cpf para varchar em scriptBanco.sql e testada a tabela usuario criada.

--- sql/scriptBanco.sql
+++ sql/scriptBanco.sql
@@ -3,7 +3,7 @@ create database askandtrust;
 \c askandtrust
 
 create table usuario (
-   CPF   integer not null constraint pk_usuario primary key,
+   CPF varchar(11) not null constraint pk_usuario primary key,
    email varchar(100) not null,
    nome varchar(100) not null,
    curso varchar(100) not null,
