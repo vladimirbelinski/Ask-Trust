d151fe256295fa8027e6a682ee436bfb59feb2b4
ketlymachado <ketly.machado@gmail.com>
2016-08-31 09:05:53 -0300

    Adicionado criação da tabela de usuário

--- sql/scriptBanco.sql
+++ sql/scriptBanco.sql
@@ -2,4 +2,11 @@ create database askandtrust;
 
 \c askandtrust
 
-create table usuario
+create table usuario (
+   CPF   integer not null constraint pk_usuario primary key,
+   email varchar(100) not null,
+   nome varchar(100) not null,
+   curso varchar(100) not null,
+   senha varchar (100) not null,
+   privilegio integer not null
+);
