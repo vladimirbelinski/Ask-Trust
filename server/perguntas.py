53bee63020afbe0f54756120132c0d334e10c0c9
João Winckler <jpwbernardi@hotmail.com>
2016-09-28 07:53:54 -0300

    Botão mágico

--- server/perguntas.py
+++ server/perguntas.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
 from bottle import run, get, post, view, request, redirect, route, static_file
 from connect import *
 
 @get('/')
 @get('/perguntas')
+@route('/perguntas', method="POST")
 @view('perguntas')
 def index():
+    perg = request.forms.perg
+    if perg != "":
+        perg = str(perg).replace("\'", "\'\'")
+        c.execute("INSERT INTO   pergunta(datahora, descricaop, userid) VALUES (now(), \'" + perg + "\', 1);");
+        conn.commit()
     c.execute("SELECT idperg, descricaop FROM pergunta")
     palavra = c.fetchall()
     return dict(palavra = palavra)
